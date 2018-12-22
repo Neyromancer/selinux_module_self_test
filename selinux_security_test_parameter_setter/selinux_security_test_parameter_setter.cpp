57a75c94ffbec01e8fd66a60b9df65e839b673e9
Neyromancer <kormulev@itsirius.su>
2018-12-22 17:59:21 +0300

    Class SelinuxSecurityTestParameterSetter added.

---
+++ selinux_security_test_parameter_setter/selinux_security_test_parameter_setter.cpp
@@ -0,0 +1,6 @@
+/// \file selinux_security_test_parameter_setter.cpp
+/// \brief
+/// \author Dmitry Kormulev <kormulev@fintech.ru>
+/// \version 1.0.0.0
+/// \date 22.12.2018
+
---
+++ selinux_security_test_parameter_setter/selinux_security_test_parameter_setter.h
@@ -0,0 +1,19 @@
+/// \file selinux_security_test_parameter_setter.h
+/// \brief
+/// \author Dmitry Kormulev <kormulev@fintech.ru>
+/// \version 1.0.0.0
+/// \date 22.12.2018
+
+#ifndef
+SECURITY_SELF_TEST_SECURITY_TESTS_SELINUX_TEST_SELINUX_SECURITY_TEST_PARAMETER_SETTER_H_
+#define file_name_guard
+
+/// \namespace fintech.
+/// \brief Область видимости проектов АО "ФИНТЕХ".
+namespace fintech {
+namespace security_self_tests {
+}  // namespace security_self_tests
+}  // namespace fintech
+
+#endif  // end of file_name_guard
+
